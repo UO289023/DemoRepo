3710babfe627c384c20df3690468dd6eb11e057e
Enol <enolllanoalonso@gmail.com>
2026-02-16 01:04:37 +0100

    Second Changes

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,7 @@
 public class Demo {
 public static void main(String[] args) {
 	System.out.println("Line 1");
-	System.out.println("Line 1");
-	System.out.println("Line 1");
+	System.out.println("Line 2");
+	System.out.println("Line 3");
 }
 }
